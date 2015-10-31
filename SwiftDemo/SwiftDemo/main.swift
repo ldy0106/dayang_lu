c6a4911753e820c066f32fc65074731180a7b2f8
卢达洋 <ludy@n2n2n.com>
2015-10-31 16:54:12 +0800

    创建SwiftDemo

---
+++ SwiftDemo/SwiftDemo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  SwiftDemo
+//
+//  Created by Lu on 15/10/31.
+//  Copyright © 2015年 Lu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World! I'm Dayang Lu.")
+
